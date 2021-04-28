25e866c6bb174913b9eac990ec961c39aa7c0a1d
dhruv <dhruv.sengupta04@gmail.com>
2021-04-28 08:44:14 -0700

    fixed Cookie

--- src/main/java/com/nighthawk/csa/Minilab/dhruvS/Inheritance/main.java
+++ src/main/java/com/nighthawk/csa/Minilab/dhruvS/Inheritance/main.java
@@ -3,7 +3,7 @@ package com.nighthawk.csa.Minilab.dhruvS.Inheritance;
 public class main {
     public static void main(String args[]) {
         Cookie x = new Cookie(10, 5);
-        Cookie.mnmCookie y = x.new mnmCookie(10, 5, 6);
+        Cookie.mnmCookie y = new Cookie.mnmCookie(10, 5, 6);
         System.out.println(y.toString());
     }
 }
